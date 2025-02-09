a7da1ecf21f90a9289dd5e9e9aca8352c0094e6f
MyNameIsJeff-305 <michelgarcia950528@icloud.com>
2025-02-09 18:01:42 -0500

    AWS S3 Setup for Cloud Storage Management

---
+++ backend/db/models/user.js
@@ -0,0 +1,66 @@
+'use strict';
+const {
+  Model, Validator
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error('Cannot be an email.');
+          }
+        },
+      },
+    },
+    firstName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: ' '
+    },
+    lastName: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: ' '
+    },
+    email: {
+      type: DataTypes.STRING(256),
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true,
+      },
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60],
+      },
+    },
+    profilePicUrl: {
+      type: DataTypes.STRING(256),
+      allowNull: false,
+      defaultValue: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+      },
+    },
+  });
+  return User;
+};
